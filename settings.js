5300e05c22b6d658949e6aa6971d1f38a9a37e6e
Lafouine2 <165853107+Lafouine2@users.noreply.github.com>
2024-08-05 23:26:36 +0100

    Add files via upload

--- settings.js
+++ settings.js
@@ -7,58 +7,58 @@
 //WhatsApp: +916909137213
 //want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
 
-const fs = require('fs')
-const chalk = require('chalk')
+const fs = require("fs");
+const chalk = require("chalk");
 
 //session
-global.sessionid ='ur session id'
+global.sessionid = "ur session id";
 
 //owmner v card
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.ytname = "YT: Xeon"; //ur yt chanel name
+global.socialm = "GitHub: Lafouine2"; //ur github or insta name
+global.location = "Africa, Cameroon, Douala"; //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
-global.websitex = "https://youtu.be/mu5m6aB6P5k"
-global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
-global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
-global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
-global.hituet = 0
+global.botname = "𝗙𝗼𝘂𝗶𝗻𝗶 | 𝗕𝗼𝘁 ✨💫"; //ur bot name
+global.ownernumber = "237651338525"; //ur owner number
+global.ownername = "ʬɸʬ 𝕃𝕠𝕣𝕕 𝔽𝕠𝕦𝕚𝕟𝕚 ʬɸʬ"; //ur owner name
+global.websitex = "https://youtu.be/filmsetdesvideos7198";
+global.wagc = "https://whatsapp.com/channel/BR0V0YskhBZ3q5umkoyhB9";
+global.themeemoji = "🪀";
+global.wm = "Xeon Bot Inc.";
+global.botscript = "https://github.com/Lafouine2/CheemsBot-MD11"; //script link
+global.packname = "Sticker By";
+global.author = "ʬɸʬ 𝕃𝕠𝕣𝕕 𝔽𝕠𝕦𝕚𝕟𝕚 ʬɸʬ\n\n+237651338525";
+global.creator = "237651338525@s.whatsapp.net";
+global.xprefix = ".";
+global.premium = ["237651338525"]; // Premium User
+global.hituet = 0;
 
 //bot sett
-global.typemenu = 'v8' // menu type 'v1' => 'v8'
-global.typereply = 'v2' // reply type 'v1' => 'v3'
-global.autoblocknumber = '92' //set autoblock country code
-global.antiforeignnumber = '91' //set anti foreign number country code
-global.welcome = false //welcome/left in groups
-global.anticall = false //bot blocks user when called
-global.autoswview = false //auto status/story view
-global.adminevent = false //show promote/demote message
-global.groupevent = false //show update messages in group chat
+global.typemenu = "v8"; // menu type 'v1' => 'v8'
+global.typereply = "v2"; // reply type 'v1' => 'v3'
+global.autoblocknumber = "92"; //set autoblock country code
+global.antiforeignnumber = "91"; //set anti foreign number country code
+global.welcome = false; //welcome/left in groups
+global.anticall = false; //bot blocks user when called
+global.autoswview = false; //auto status/story view
+global.adminevent = false; //show promote/demote message
+global.groupevent = false; //show update messages in group chat
 //msg
 global.mess = {
-	limit: 'Your limit is up!',
-	nsfw: 'Nsfw is disabled in this group, Please tell the admin to enable',
-    done: 'Done✓',
-    error: 'Error!',
-    success: 'Here you go!'
-}
+  limit: "Your limit is up!",
+  nsfw: "Nsfw is disabled in this group, Please tell the admin to enable",
+  done: "Done✓",
+  error: "Error!",
+  success: "Here you go!",
+};
 //thumbnail
-global.thumb = fs.readFileSync('./XeonMedia/theme/cheemspic.jpg')
+global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg");
 
-let file = require.resolve(__filename)
+let file = require.resolve(__filename);
 fs.watchFile(file, () => {
-    fs.unwatchFile(file)
-    console.log(chalk.redBright(`Update'${__filename}'`))
-    delete require.cache[file]
-    require(file)
-})
+  fs.unwatchFile(file);
+  console.log(chalk.redBright(`Update'${__filename}'`));
+  delete require.cache[file];
+  require(file);
+});
